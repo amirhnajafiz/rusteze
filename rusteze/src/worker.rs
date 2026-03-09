ca9760f25a04fa9d416a3fc4dd193f6f617a8f41
amirhnajafiz <najafizadeh21@gmail.com>
2026-03-09 19:35:28 -0400

    feat: snapshot last 5 keep

--- rusteze/src/worker.rs
+++ rusteze/src/worker.rs
@@ -38,5 +38,31 @@ pub async fn worker_snapshot(seconds: u64, path: String, memcache: Arc<Mutex<Mem
         }
 
         info!("snapshot taken and save to file {}", filename);
+
+        // remove old snapshots, keep only the latest 5 snapshots
+        let mut snapshots = std::fs::read_dir(&path)
+            .unwrap()
+            .filter_map(|entry| {
+                let entry = entry.unwrap();
+                let filename = entry.file_name().into_string().unwrap();
+                if filename.starts_with("snapshot_") && filename.ends_with(".json") {
+                    Some((entry.path(), entry.metadata().unwrap().created().unwrap()))
+                } else {
+                    None
+                }})
+            .collect::<Vec<_>>();
+
+        snapshots.sort_by_key(|k| k.1);
+
+        let snapshots_len = snapshots.len();
+        if snapshots_len > 5 {
+            for (path, _) in snapshots.into_iter().take(snapshots_len - 5) {
+                std::fs::remove_file(path).unwrap_or_else(|e| {
+                    info!("failed to remove old snapshot: {}", e);
+                });
+            }
+
+            info!("old snapshots removed, kept only the latest 5 snapshots");
+        }
     }
 }
